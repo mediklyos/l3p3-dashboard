81ae077f8978eba988dae6700a2abe3e750c48f2
Federico Fdez <fa.fernandez@alumnos.upm.es>
2014-07-22 09:20:26 +0200

    Improved color palette

--- dashboard/js/src/color_map.js
+++ dashboard/js/src/color_map.js
@@ -4,12 +4,16 @@ function eventWindowLoaded() {
 }
 function add_coloring_book_events() {
     $('#submit').bind("click",function(event){
-        number = $('#number_choice').val();
-        if(number>50){
-            color_chosen = 'rgb(234,86,86)';
+        var colors = ["#F9AFAD","#F99EA3","#F9827F","#F9848E","#FC6675",
+            "#F95E59","#FC4F59","#F43F4F","#EF2B2D","#D62100",
+            "#E23D28","#D62100","#D62828","#CC2D30","#C13828",
+            "#AF2626","#A03033","#7C2D23","#7C211E","#5B2D28"];
+        var number = $('#number_choice').val();
+        if(number>=0 && number<=100){
+            color_chosen = colors[number/10];
         }
         else{
-            color_chosen = 'rgb(206,52,52)';
+            color_chosen = 'rgb(0,0,0)';
         }
         $('rect[class="colorable"]').attr("fill",color_chosen);
     });
