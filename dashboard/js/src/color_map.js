fb77dd5fbc88444d853d36c030161624e0b64e83
Federico Fdez <fa.fernandez@alumnos.upm.es>
2014-07-22 10:44:01 +0200

    fill(stats) function added

--- dashboard/js/src/color_map.js
+++ dashboard/js/src/color_map.js
@@ -2,6 +2,7 @@ window.addEventListener('load', eventWindowLoaded(), false);
 function eventWindowLoaded() {
     add_coloring_book_events();
 }
+
 function add_coloring_book_events() {
     $('#submit').bind("click",function(event){
         var colors = ["#F9AFAD","#F99EA3","#F9827F","#F9848E","#FC6675",
@@ -9,14 +10,31 @@ function add_coloring_book_events() {
             "#E23D28","#D62100","#D62828","#CC2D30","#C13828",
             "#AF2626","#A03033","#7C2D23","#7C211E","#5B2D28"];
         var number = $('#number_choice').val();
-        if(number>=0 && number<=100){
-            color_chosen = colors[number/10];
+        if(number>=0 && number<100){
+            color_chosen = colors[Math.floor(number/5)];
         }
         else{
-            color_chosen = 'rgb(0,0,0)';
+            number==100 ? color_chosen=colors[colors.length] : color_chosen = 'rgb(0,0,0)';
         }
         $('rect[class="colorable"]').attr("fill",color_chosen);
     });
+    $('#reset_image').bind("click", function(event) {
+        $('rect[class="colorable"]').attr("fill", "white");
+    });
+}
+
+function fill(stats) {
+    var colors = ["#F9AFAD","#F99EA3","#F9827F","#F9848E","#FC6675",
+        "#F95E59","#FC4F59","#F43F4F","#EF2B2D","#D62100",
+        "#E23D28","#D62100","#D62828","#CC2D30","#C13828",
+        "#AF2626","#A03033","#7C2D23","#7C211E","#5B2D28"];
+    if(stats>=0 && stats<100){
+        color_chosen = colors[Math.floor(number/5)];
+    }
+    else{
+        stats==100 ? color_chosen=colors[colors.length] : color_chosen = 'rgb(0,0,0)';
+    }
+    $('rect[class="colorable"]').attr("fill",color_chosen);
     $('#reset_image').bind("click", function(event) {
         $('rect[class="colorable"]').attr("fill", "white");
     });
