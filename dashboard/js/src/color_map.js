18157667a5a98c5d39a569ddca14d76df2d760d5
Federico Fdez <fa.fernandez@alumnos.upm.es>
2014-07-23 10:44:24 +0200

    Improvements in ColorMap (again)
    Switch loop removed

--- dashboard/js/src/color_map.js
+++ dashboard/js/src/color_map.js
@@ -15,34 +15,25 @@ function stream() {
         console.log('disconnect');
     };
     ws.onmessage = function (message) {
+        var whitespaces;
+        var line = message.data.trim().toString();
+        //Count whitespaces in message.data
 
-        if(message.trim())
+        line.indexOf(' ')==-1 ? whitespaces=false : whitespaces = true;
 
-        switch (lineCount++) {
-        case 0:
-            console.log("Esto debería ser el nombre del servidor: "+message);
-        break;
-
-        case 1: // column headings
-          colHeadings = message.data.trim().split(/ +/);
-        break;
-
-        case 2://Recurso de Color Map
-          recurso=message;
-        break;
-
-        default: // subsequent lines
-            var colValues = message.data.trim().split(/ +/);
+        console.log(line.charAt(0)+' '+isNaN(parseInt(line.charAt(0))));
+        if(!isNaN(parseInt(line.charAt(0)))){
+            //most typical case: numbers
+            var colValues = line.split(/ +/);
             var stats = {};
             var colors = ["#F9AFAD","#F99EA3","#F9827F","#F9848E","#FC6675",
                 "#F95E59","#FC4F59","#F43F4F","#EF2B2D","#D62100",
                 "#E23D28","#D62100","#D62828","#CC2D30","#C13828",
                 "#AF2626","#A03033","#7C2D23","#7C211E","#5B2D28"];
-            console.log(recurso.data);
+            console.log(recurso);
             for (var i = 0; i < colHeadings.length; i++) {
-                if(colHeadings[i].toString()==recurso.data){
+                if(colHeadings[i].toString()==recurso){
                     stats=colValues[i];
-
                     if(stats>=0 && stats<100){
                         color_chosen = colors[Math.floor(stats/5)];
                     }
@@ -52,9 +43,16 @@ function stream() {
                     $('rect[class="colorable"]').attr("fill",color_chosen);
                 }}
             console.log(stats);
-
-    }
-
+        }
+        else if(!whitespaces && line.length!=0){
+            //it is a resource
+            console.log("Esto debería ser el nombre del recurso: "+line);
+            recurso=line;
+        }
+        else {
+            //column headings
+            colHeadings = line.split(/ +/);
+        }
     };
 }
     stream();
