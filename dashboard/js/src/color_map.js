5f143980dbe7bad88e63500ad0246e06f0393e72
Federico Fdez <fa.fernandez@alumnos.upm.es>
2014-07-23 12:48:50 +0200

    Improved performance.
    ColorMap working properly (at last).

--- dashboard/js/src/color_map.js
+++ dashboard/js/src/color_map.js
@@ -1,18 +1,12 @@
 function stream() {
 
     var ws = new ReconnectingWebSocket('ws://' + location.hostname + ':8080');
-    var lineCount;
-    var colHeadings;
-    var recurso;
-    var num_space=0;
-    var flag_cabeceras=0;
+    var lineCount, colHeadings, resource;
 
     ws.onopen = function () {
         console.log('connect');
         ws.send("Color Map request");
         lineCount=0;
-        flag_cabeceras=0;
-
     };
 
     ws.onclose = function () {
@@ -21,19 +15,17 @@ function stream() {
     ws.onmessage = function (message) {
         var whitespaces;
         var line = message.data.trim().toString();
-        //Count whitespaces in message.data
-
+        //Are there whitespaces in the received line?
         line.indexOf(' ')==-1 ? whitespaces=false : whitespaces = true;
 
         if(!isNaN(parseInt(line.charAt(0)))){
-            //most typical case: numbers
-
-            paint(recurso,colHeadings,line);
+            //most typical case (number) first to perform faster: if the first character is a number
+            paint(resource,colHeadings,line);
         }
         else if(!whitespaces && line.length!=0){
-            //it is a resource
-            recurso=line;
-            $("#recurso").text(recurso);
+            //resource name (no whitespaces and no blank line)
+            resource=line;
+            $("#resource").text(resource);
         }
         else {
             //column headings
@@ -44,27 +36,27 @@ function stream() {
 
 function paint(recurso,colHeadings,line){
 
-
     var colValues = line.split(/ +/);
     var stats = {};
+    //Range of 20 colors from light red to dark red
     var colors = ["#F9AFAD","#F99EA3","#F9827F","#F9848E","#FC6675",
         "#F95E59","#FC4F59","#F43F4F","#EF2B2D","#D62100",
         "#E23D28","#D62100","#D62828","#CC2D30","#C13828",
         "#AF2626","#A03033","#7C2D23","#7C211E","#5B2D28"];
     for (var i = 0; i < colHeadings.length; i++) {
+        //take only the data of the source to be shown in the ColorMap
         if(colHeadings[i].toString()==recurso){
             stats=colValues[i];
-
             if(stats>0 && stats<100){
                 color_chosen = colors[Math.floor(stats/5)];
             }
             else{
+                //if 100, take last color, else take black
                 stats==100 ? color_chosen=colors[colors.length] : color_chosen = 'rgb(0,0,0)';
             }
             $('rect[class="colorable"]').attr("fill",color_chosen);
         }
     }
-
 }
     stream();
 
