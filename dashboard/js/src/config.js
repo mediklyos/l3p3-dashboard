58034b200b77135a168775f27250bc3d276df40e
Francisco Huertas <francisco@fhuertas.com>
2014-07-31 17:50:34 +0200

    stable config

--- dashboard/js/src/config.js
+++ dashboard/js/src/config.js
@@ -12,6 +12,6 @@ views[2] = []
 
 views[0][0]  = {id: "DynamicDistributions",constantsPrefix: "dd",ref: "js/templates/template_dynamicDistribution.ejs",title : "Distribution charts"}
 views[0][1]  = {id: "ComputerResources",constantsPrefix: "dd",ref: "js/templates/template_comp_resources.ejs",title : "Online Data Viewer"}
-views[1][0] = {id: "SingleNodeViewer",constantsPrefix: "snv",ref: "js/templates/template_singleNodeViewer.ejs",title : "Single Node Viewer"}
-views[1][1] = {id: "GlobalEventsViewer",constantsPrefix: "gev",ref: "js/templates/template_globalEventsViewer.ejs",title: "Global Events Viewer"}
-views[2][0] = {id:"examples",constantsPrefix: "",ref: "js/templates/example.ejs",title: "Examples"}
+//views[1][0] = {id: "SingleNodeViewer",constantsPrefix: "snv",ref: "js/templates/template_singleNodeViewer.ejs",title : "Single Node Viewer"}
+//views[1][1] = {id: "GlobalEventsViewer",constantsPrefix: "gev",ref: "js/templates/template_globalEventsViewer.ejs",title: "Global Events Viewer"}
+//views[2][0] = {id:"examples",constantsPrefix: "",ref: "js/templates/example.ejs",title: "Examples"}
