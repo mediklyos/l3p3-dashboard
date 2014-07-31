5a844df0ad0cef6998b40fbd16b74aa3af76bdb9
Francisco Huertas <francisco@fhuertas.com>
2014-07-31 17:30:06 +0200

    config

--- dashboard/js/src/config.js
+++ dashboard/js/src/config.js
@@ -14,4 +14,4 @@ views[0][0]  = {id: "DynamicDistributions",constantsPrefix: "dd",ref: "js/templa
 views[0][1]  = {id: "ComputerResources",constantsPrefix: "dd",ref: "js/templates/template_comp_resources.ejs",title : "Online Data Viewer"}
 //views[1][0] = {id: "SingleNodeViewer",constantsPrefix: "snv",ref: "js/templates/template_singleNodeViewer.ejs",title : "Single Node Viewer"}
 //views[1][1] = {id: "GlobalEventsViewer",constantsPrefix: "gev",ref: "js/templates/template_globalEventsViewer.ejs",title: "Global Events Viewer"}
-//views[2][0] = {id:"examples",constantsPrefix: "",ref: "js/templates/example.ejs",title: "Examples"}
+//views[2][0] = {id:"examples",constantsPrefix: "",ref: "js/templates/example.ejs",title: "Examples"}//prueba
