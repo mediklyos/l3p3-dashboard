6bc265efaf563f3ffe6513058e7881100b97e45d
Francisco Huertas <francisco@fhuertas.com>
2014-08-06 16:27:41 +0200

    Added the server for OnlineDataViewer

---
+++ onlinedataviewerserver/README.md
@@ -0,0 +1,61 @@
+﻿
+l3p3_dashboard_serv_node
+========================
+
+Jorge Ramírez Carrasco and Federico A. Fernández Moreno
+* * *
+
+Description
+--------
+Handles connections of both Computer Resources and ColorMap tabs.
+
+How it works
+--------
+When a connection is established, no matter from what tab, the server starts sending data through that connection.
+
+It also handles the messages received by the client:
+
+* If a number is received, the server will take it as a timeout value, and will change its sending-data rate consequently.
+* If a *Color Map Request* string is received (the client sends this when the ColorMap tag is entered), the server will send the desired resource to be represented, if there is one (remember that the user must have selected one before, otherwise the ColorMap will not work).
+* Finally, if a different string is received, the server will take it as a resource id, and store it. The id will also be broadcast to all websockets
+* * *
+
+Input format
+--------
+
+Server requires the input data with these charactheristics.
+
+    -Format: “.csv”.
+    -First line: Headings ended with node number.
+    -Separation: Backspace.
+
+Headings ended with the same number may be interpreted by the client as the same chart.
+Headings may be given in each order.
+Headings not given in the first line may result in the client ignoring the data before the headings.
+
+* * *
+
+Required Libraries
+--------
+
+The following libraries have been added:
+
+  **Server (npm libraries)**: 
+
+      fs("filesystem")  
+      ws("websocket")
+
+  **Client**:
+
+      sugar-1.4.1.min.js
+      jquery-2.0.3.min.js
+      chroma.min.js
+      
+Moreover to run graphics is necessary the following code available in his author repository.
+
+      smoothie.js [link to the author's repo](https://github.com/joewalnes/smoothie.git)
+      reconnecting-websocket.js [link to the author's repo](https://github.com/joewalnes/reconnecting-websocket.git)
+
+Note that the version of *reconnecting-websocket.js* here is a modified version of the original.
+
+* * *
---
+++ onlinedataviewerserver/server.js
@@ -0,0 +1,70 @@
+var TIME_OUT = 500;
+
+console.log("hola mundo")
+
+
+
+var fs = require('fs');
+var WebSocketServer = require('ws').Server
+    , wss = new WebSocketServer({port: 8080});
+
+    var ip, resource;
+    var websockets=[];
+    var line_csv=1;
+    var timeoutFunction = function (ws,line,data) {
+        ip =ws.upgradeReq.connection.remoteAddress;
+        console.log("Sending data to  " + ip + "   :" + data[line]);
+        if (!ws.isClose) {
+            ws.send(data[line]);
+            line++;
+            line_csv=line;
+            if(line==data.length){
+                line=1;
+            }
+            setTimeout(timeoutFunction.bind(this,ws,line,data),TIME_OUT)
+        }
+        //try {
+        //} catch (e) {
+        //    console.log("Exceptio catched: " + e)
+        //}
+    }
+
+    wss.on('connection', function (ws) {
+        ws.isClose = false;
+        websockets.push(ws);
+        ws.send('');
+        var route = "nodes.csv"; //path to the .csv file to broadcast
+        fs.readFile(route, 'utf8', function (err, data) {
+            var data2 = data.split("\n");
+            ws.send(data2[0]);
+            setTimeout(timeoutFunction.bind(this,ws,line_csv,data2),TIME_OUT);
+        });
+        ws.on('message', function(message) {
+            console.log('received: %s', message);
+            if(!isNaN(parseFloat(message)) && isFinite(message)) {
+                //if a number is received, then it is a new timeout value
+                TIME_OUT = message * 10;
+            }
+            else if(message!="Color Map request"){
+                //if no ColorMap request, then a resource name has been received
+                resource=message;
+                console.log("Resource "+resource+" has been stored");
+                 for(var i=0;i<websockets.length;i++)
+                     //broadcasts the new resource to all machines
+                     if (websockets[i].isClose) {
+                         console.log("size" + console.log)
+                         websockets.splice(i, 1);
+                     } else {
+                         websockets[i].send(resource);
+                     }
+            }
+            else if(resource!=undefined){
+                //if a ColorMap request if received, send the resource if there is one (else does nothing)
+                ws.send(resource);
+                console.log("Resource "+resource+" has been sent");
+            }
+        });
+        ws.on('close', function (message) {
+            ws.isClose = true;
+        });
+    });
