e4ae74e8cea76373e625f2774388e393c311cca0
Mario Pérez Esteso <mario@geekytheory.com>
2014-11-26 10:53:05 +0100

    Fixed bug that was ignoring a step at the end of the simulation.

--- dashboard/js/src/bootstrapPrefabs.js
+++ dashboard/js/src/bootstrapPrefabs.js
@@ -1,20 +1,3 @@
-jQuery(function ($) {
-    $('.panel-heading span.clickable').on("click", function (e) {
-        if ($(this).hasClass('panel-collapsed')) {
-            // expand the panel
-            $(this).parents('.panel').find('.panel-body').slideDown();
-            $(this).removeClass('panel-collapsed');
-            $(this).find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        }
-        else {
-            // collapse the panel
-            $(this).parents('.panel').find('.panel-body').slideUp();
-            $(this).addClass('panel-collapsed');
-            $(this).find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-        }
-    });
-});
-
 function createSetFilterPanelsByAttributeValue(id,parent,panelList,panel_id_prefix,panels_class,callback,inline){
     if (inline === undefined){
         inline = true;
